57a2eb8ba9c7d21b475a832e1d1ca14f6498071f
woongstar <qualtypo@gmail.com>
2022-04-06 16:09:09 +0900

    dote backend init

---
+++ src/test/java/Kafry/Dote/DoteApplicationTests.java
@@ -0,0 +1,13 @@
+package Kafry.Dote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
